92c0778796d1ed15b2a92ca825dc2f37f5d8f9a5
AutoCode <autocode@papergo.io>
2024-06-28 01:42:09 +0300

    update from autocode on 2024-06-28 01:42:09.953873

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -80,16 +80,7 @@ namespace ProjectName.Controllers
             }
             catch (Exception ex)
             {
-                var exceptionResponse = new Response<object>
-                {
-                    Exception = new ExceptionInfo
-                    {
-                        Id = Guid.NewGuid().ToString(),
-                        Code = ex is BusinessException || ex is TechnicalException ? ex.GetType().Name : "1001",
-                        Description = ex is BusinessException || ex is TechnicalException ? ex.Message : "A technical exception has occurred, please contact your system administrator"
-                    }
-                };
-                return new OkObjectResult(exceptionResponse);
+                return new OkObjectResult(new Response<object> { Exception = new ExceptionInfo { Id = Guid.NewGuid().ToString(), Code = "Technical", Description = ex.Message } });
             }
         }
     }
@@ -117,18 +108,6 @@ namespace ProjectName.Controllers
     public class ExceptionInfo
     {
         public string Id { get; set; }
-        public string Code { get; set; }
-        public string Description { get; set; }
-    }
-
-    public class BusinessException : Exception
-    {
-        public string Code { get; set; }
-        public string Description { get; set; }
-    }
-
-    public class TechnicalException : Exception
-    {
         public string Code { get; set; }
         public string Description { get; set; }
     }
--- obj/Debug/net6.0/ProjectName.AssemblyInfo.cs
+++ obj/Debug/net6.0/ProjectName.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectName")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60e4c3c83c2c9c053d86b0c208bdf9727d8f6107")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectName")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectName")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

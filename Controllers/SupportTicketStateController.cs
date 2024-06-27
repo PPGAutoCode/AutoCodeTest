aa5b09b051731e4d7294149929bd341139af02c2
AutoCode <autocode@papergo.io>
2024-06-28 01:44:52 +0300

    update from autocode on 2024-06-28 01:44:52.375618

--- Controllers/SupportTicketStateController.cs
+++ Controllers/SupportTicketStateController.cs
@@ -64,4 +64,60 @@ namespace ProjectName.Controllers
         {
             return await SafeExecutor.ExecuteAsync(async () =>
             {
-                var result = 
+                var result = await _supportTicketStateService.GetListSupportTicketState(request.Payload);
+                return Ok(new Response<List<SupportTicketState>> { Payload = result });
+            });
+        }
+    }
+
+    public static class SafeExecutor
+    {
+        public static async Task<IActionResult> ExecuteAsync(Func<Task<IActionResult>> action)
+        {
+            try
+            {
+                return await action();
+            }
+            catch (Exception ex)
+            {
+                var exceptionResponse = new Response<object>
+                {
+                    Exception = new ExceptionInfo
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Code = "1001",
+                        Description = "A technical exception has occurred, please contact your system administrator"
+                    }
+                };
+                return new OkObjectResult(exceptionResponse);
+            }
+        }
+    }
+
+    public class Request<T>
+    {
+        public Header Header { get; set; }
+        public T Payload { get; set; }
+    }
+
+    public class Response<T>
+    {
+        public T Payload { get; set; }
+        public ExceptionInfo Exception { get; set; }
+    }
+
+    public class ExceptionInfo
+    {
+        public string Id { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class Header
+    {
+        public string ID { get; set; }
+        public string Application { get; set; }
+        public string Bank { get; set; }
+        public string UserId { get; set; }
+    }
+}

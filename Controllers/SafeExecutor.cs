99f9a8706a4ec7402dcd78e8a3e220ac8151cb4b
AutoCode <autocode@papergo.io>
2024-06-28 01:22:47 +0300

    update from autocode on 2024-06-28 01:22:47.949629

---
+++ Controllers/SafeExecutor.cs
@@ -0,0 +1,56 @@
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectName.Controllers
+{
+    public static class SafeExecutor
+    {
+        public static async Task<IActionResult> ExecuteAsync(Func<Task<IActionResult>> action)
+        {
+            try
+            {
+                return await action();
+            }
+            catch (Exception ex)
+            {
+                var exceptionResponse = new Response<object>
+                {
+                    Exception = new ExceptionInfo
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Code = ex is BusinessException || ex is TechnicalException ? ex.Code : "1001",
+                        Description = ex is BusinessException || ex is TechnicalException ? ex.Description : "A technical exception has occurred, please contact your system administrator"
+                    }
+                };
+                return new OkObjectResult(exceptionResponse);
+            }
+        }
+    }
+
+    public class Response<T>
+    {
+        public T Payload { get; set; }
+        public ExceptionInfo Exception { get; set; }
+    }
+
+    public class ExceptionInfo
+    {
+        public string Id { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class BusinessException : Exception
+    {
+        public string Code { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class TechnicalException : Exception
+    {
+        public string Code { get; set; }
+        public string Description { get; set; }
+    }
+}

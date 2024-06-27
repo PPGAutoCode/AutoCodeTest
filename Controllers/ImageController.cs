be81c0c0be7d377a70f52d7f23908aa1c6eeffaf
AutoCode <autocode@papergo.io>
2024-06-28 01:36:18 +0300

    update from autocode on 2024-06-28 01:36:18.051071

--- Controllers/ImageController.cs
+++ Controllers/ImageController.cs
@@ -1,11 +1,8 @@
-It appears that the error message indicates a missing semicolon at line 90, column 115 in the `ImageController.cs` file. Let's review the code around that line to identify and fix the issue.
 
-Here's the corrected code with the missing semicolon added:
-
-```csharp
 using Microsoft.AspNetCore.Mvc;
 using ProjectName.Types;
 using ProjectName.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -23,68 +20,115 @@ namespace ProjectName.Controllers
         }
 
         [HttpPost("create")]
-        public async Task<IActionResult> CreateImage([FromBody] CreateImageDto createImageDto)
+        public async Task<IActionResult> CreateImage([FromBody] Request<CreateImageDto> request)
         {
             return await SafeExecutor.ExecuteAsync(async () =>
             {
-                var result = await _imageService.CreateImage(createImageDto);
-                return Ok(result);
+                var result = await _imageService.CreateImage(request.Payload);
+                return Ok(new Response<string> { Payload = result });
             });
         }
 
         [HttpPost("get")]
-        public async Task<IActionResult> GetImage([FromBody] ImageRequestDto imageRequestDto)
+        public async Task<IActionResult> GetImage([FromBody] Request<ImageRequestDto> request)
         {
             return await SafeExecutor.ExecuteAsync(async () =>
             {
-                var result = await _imageService.GetImage(imageRequestDto);
-                return Ok(result);
+                var result = await _imageService.GetImage(request.Payload);
+                return Ok(new Response<Image> { Payload = result });
             });
         }
 
         [HttpPost("update")]
-        public async Task<IActionResult> UpdateImage([FromBody] UpdateImageDto updateImageDto)
+        public async Task<IActionResult> UpdateImage([FromBody] Request<UpdateImageDto> request)
         {
             return await SafeExecutor.ExecuteAsync(async () =>
             {
-                var result = await _imageService.UpdateImage(updateImageDto);
-                return Ok(result);
+                var result = await _imageService.UpdateImage(request.Payload);
+                return Ok(new Response<string> { Payload = result });
             });
         }
 
         [HttpPost("delete")]
-        public async Task<IActionResult> DeleteImage([FromBody] DeleteImageDto deleteImageDto)
+        public async Task<IActionResult> DeleteImage([FromBody] Request<DeleteImageDto> request)
         {
             return await SafeExecutor.ExecuteAsync(async () =>
             {
-                var result = await _imageService.DeleteImage(deleteImageDto);
-                return Ok(result);
+                var result = await _imageService.DeleteImage(request.Payload);
+                return Ok(new Response<bool> { Payload = result });
             });
         }
 
         [HttpPost("list")]
-        public async Task<IActionResult> GetListImage([FromBody] ListImageRequestDto listImageRequestDto)
+        public async Task<IActionResult> GetListImage([FromBody] Request<ListImageRequestDto> request)
         {
             return await SafeExecutor.ExecuteAsync(async () =>
             {
-                var result = await _imageService.GetListImage(listImageRequestDto);
-                return Ok(result);
+                var result = await _imageService.GetListImage(request.Payload);
+                return Ok(new Response<List<Image>> { Payload = result });
             });
         }
     }
 
-    [ApiController]
-    [Route("api/[controller]")]
-    public class BlogTagController : ControllerBase
+    public static class SafeExecutor
     {
-        private readonly IBlogTagService _blogTagService;
-
-        public BlogTagController(IBlogTagService blogTagService)
+        public static async Task<IActionResult> ExecuteAsync(Func<Task<IActionResult>> action)
         {
-            _blogTagService = blogTagService;
+            try
+            {
+                return await action();
+            }
+            catch (Exception ex)
+            {
+                return new OkObjectResult(new Response<object>
+                {
+                    Exception = new ExceptionInfo
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Code = ex is BusinessException || ex is TechnicalException ? ex.GetType().Name : "1001",
+                        Description = ex is BusinessException || ex is TechnicalException ? ex.Message : "A technical exception has occurred, please contact your system administrator"
+                    }
+                });
+            }
         }
     }
-}
-```
 
-I have added the missing semicolon at the specified location. This should resolve the compilation error without altering the logic of the code.
+    public class Response<T>
+    {
+        public T Payload { get; set; }
+        public ExceptionInfo Exception { get; set; }
+    }
+
+    public class ExceptionInfo
+    {
+        public string Id { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class Request<T>
+    {
+        public Header Header { get; set; }
+        public T Payload { get; set; }
+    }
+
+    public class Header
+    {
+        public string ID { get; set; }
+        public string Application { get; set; }
+        public string Bank { get; set; }
+        public string UserId { get; set; }
+    }
+
+    public class BusinessException : Exception
+    {
+        public string Code { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class TechnicalException : Exception
+    {
+        public string Code { get; set; }
+        public string Description { get; set; }
+    }
+}
--- obj/Debug/net6.0/ProjectName.AssemblyInfo.cs
+++ obj/Debug/net6.0/ProjectName.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectName")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b429952b29a9cb9c7bbe24bd66d33fd3bfa8b5aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectName")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectName")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

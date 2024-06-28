b77a277008ceba8d7c3e218b23cf573d9aea6ecb
AutoCode <autocode@papergo.io>
2024-06-28 19:03:28 +0300

    update from autocode on 2024-06-28 19:03:28.724962

--- Controllers/SelectTicketPriorityController.cs
+++ Controllers/SelectTicketPriorityController.cs
@@ -2,9 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using ProjectName.Types;
 using ProjectName.Interfaces;
-using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace ProjectName.Controllers
 {
@@ -12,61 +11,61 @@ namespace ProjectName.Controllers
     [Route("api/[controller]")]
     public class SelectTicketPriorityController : ControllerBase
     {
-        private readonly ISelectTicketPriorityService _service;
+        private readonly ISelectTicketPriorityService _selectTicketPriorityService;
 
-        public SelectTicketPriorityController(ISelectTicketPriorityService service)
+        public SelectTicketPriorityController(ISelectTicketPriorityService selectTicketPriorityService)
         {
-            _service = service;
+            _selectTicketPriorityService = selectTicketPriorityService;
         }
 
         [HttpPost("create")]
         public async Task<IActionResult> CreateSelectTicketPriority([FromBody] Request<CreateSelectTicketPriorityDto> request)
         {
-            return await SafeExecutor.ExecuteAsync(async () =>
+            return Ok(await SafeExecutor.ExecuteAsync(async () =>
             {
-                var result = await _service.CreateSelectTicketPriority(request.Payload);
-                return Ok(new Response<string> { Payload = result });
-            });
+                var result = await _selectTicketPriorityService.CreateSelectTicketPriority(request.Payload);
+                return new Response<string> { Payload = result };
+            }));
         }
 
         [HttpPost("get")]
         public async Task<IActionResult> GetSelectTicketPriority([FromBody] Request<SelectTicketPriorityRequestDto> request)
         {
-            return await SafeExecutor.ExecuteAsync(async () =>
+            return Ok(await SafeExecutor.ExecuteAsync(async () =>
             {
-                var result = await _service.GetSelectTicketPriority(request.Payload);
-                return Ok(new Response<SelectTicketPriority> { Payload = result });
-            });
+                var result = await _selectTicketPriorityService.GetSelectTicketPriority(request.Payload);
+                return new Response<SelectTicketPriority> { Payload = result };
+            }));
         }
 
         [HttpPost("update")]
         public async Task<IActionResult> UpdateSelectTicketPriority([FromBody] Request<UpdateSelectTicketPriorityDto> request)
         {
-            return await SafeExecutor.ExecuteAsync(async () =>
+            return Ok(await SafeExecutor.ExecuteAsync(async () =>
             {
-                var result = await _service.UpdateSelectTicketPriority(request.Payload);
-                return Ok(new Response<string> { Payload = result });
-            });
+                var result = await _selectTicketPriorityService.UpdateSelectTicketPriority(request.Payload);
+                return new Response<string> { Payload = result };
+            }));
         }
 
         [HttpPost("delete")]
         public async Task<IActionResult> DeleteSelectTicketPriority([FromBody] Request<DeleteSelectTicketPriorityDto> request)
         {
-            return await SafeExecutor.ExecuteAsync(async () =>
+            return Ok(await SafeExecutor.ExecuteAsync(async () =>
             {
-                var result = await _service.DeleteSelectTicketPriority(request.Payload);
-                return Ok(new Response<bool> { Payload = result });
-            });
+                var result = await _selectTicketPriorityService.DeleteSelectTicketPriority(request.Payload);
+                return new Response<bool> { Payload = result };
+            }));
         }
 
         [HttpPost("list")]
         public async Task<IActionResult> GetListSelectTicketPriority([FromBody] Request<ListSelectTicketPriorityRequestDto> request)
         {
-            return await SafeExecutor.ExecuteAsync(async () =>
+            return Ok(await SafeExecutor.ExecuteAsync(async () =>
             {
-                var result = await _service.GetGetListSelectTicketPriority(request.Payload);
-                return Ok(new Response<List<SelectTicketPriority>> { Payload = result });
-            });
+                var result = await _selectTicketPriorityService.GetListSelectTicketPriority(request.Payload);
+                return new Response<List<SelectTicketPriority>> { Payload = result };
+            }));
         }
     }
 }

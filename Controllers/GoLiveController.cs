a1ad15ceeac00016b909b5bbaf43d4c348cc7dfd
AutoCode <autocode@papergo.io>
2024-06-28 19:06:05 +0300

    update from autocode on 2024-06-28 19:06:05.205401

--- Controllers/GoLiveController.cs
+++ Controllers/GoLiveController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Mvc;
 using ProjectName.Types;
 using ProjectName.Interfaces;
-using System;
 using System.Threading.Tasks;
 
 namespace ProjectName.Controllers
@@ -23,57 +22,9 @@ namespace ProjectName.Controllers
         {
             return await SafeExecutor.ExecuteAsync(async () =>
             {
-                var result = await _goLiveService.CreateGoLive(request.Payload);
+                string result = await _goLiveService.CreateGoLive(request.Payload);
                 return Ok(new Response<string> { Payload = result });
             });
         }
-    }
-
-    public static class SafeExecutor
-    {
-        public static async Task<IActionResult> ExecuteAsync(Func<Task<IActionResult>> action)
-        {
-            try
-            {
-                return await action();
-            }
-            catch (BusinessException ex)
-            {
-                return new OkObjectResult(new Response<object> { Exception = new ExceptionInfo { Id = Guid.NewGuid().ToString(), Code = ex.Code, Description = ex.Description } });
-            }
-            catch (TechnicalException ex)
-            {
-                return new OkObjectResult(new Response<object> { Exception = new ExceptionInfo { Id = Guid.NewGuid().ToString(), Code = ex.Code, Description = ex.Description } });
-            }
-            catch (Exception)
-            {
-                return new OkObjectResult(new Response<object> { Exception = new ExceptionInfo { Id = Guid.NewGuid().ToString(), Code = "1001", Description = "A technical exception has occurred, please contact your system administrator" } });
-            }
-        }
-    }
-
-    public class Response<T>
-    {
-        public T Payload { get; set; }
-        public ExceptionInfo Exception { get; set; }
-    }
-
-    public class ExceptionInfo
-    {
-        public string Id { get; set; }
-        public string Code { get; set; }
-        public string Description { get; set; }
-    }
-
-    public class BusinessException : Exception
-    {
-        public string Code { get; set; }
-        public string Description { get; set; }
-    }
-
-    public class TechnicalException : Exception
-    {
-        public string Code { get; set; }
-        public string Description { get; set; }
     }
 }
--- obj/Debug/net6.0/ProjectName.AssemblyInfo.cs
+++ obj/Debug/net6.0/ProjectName.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectName")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8c7c3f79990e33bebff54b27a88000e83f4f52d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectName")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectName")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

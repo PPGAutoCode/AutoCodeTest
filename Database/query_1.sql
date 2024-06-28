865bb9c8f2147f387351757d8397b2eb8781781a
AutoCode <autocode@papergo.io>
2024-06-28 18:32:51 +0300

    update from autocode on 2024-06-28 18:32:51.241567

--- Database/query_1.sql
+++ Database/query_1.sql
@@ -1,11 +1,19 @@
 
--- File: ProductCategories.sql
-CREATE TABLE ProductCategories (
-    Id UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
-    Name NVARCHAR(200) NOT NULL UNIQUE,
-    UserQuestionnaire BIT NULL,
-    Description NVARCHAR(MAX) NULL,
-    Parent NVARCHAR(MAX) NULL,
-    UrlAlias NVARCHAR(200) NULL,
-    Weight INT NULL
+-- File: UserQuestionnaire.sql
+CREATE TABLE UserQuestionnaire (
+    id UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
+    CompanyErpSolutionName NVARCHAR(255) NOT NULL UNIQUE,
+    CompanyErpSolutionVersion NVARCHAR(255) NOT NULL UNIQUE,
+    CompanyKad NVARCHAR(255) NOT NULL UNIQUE,
+    CompanyLegalName NVARCHAR(255) NOT NULL UNIQUE,
+    CompanyOwnsBankAccount BIT NOT NULL UNIQUE,
+    CompanyReprEmail NVARCHAR(255) NOT NULL UNIQUE,
+    CompanyRepFullName NVARCHAR(255) NOT NULL UNIQUE,
+    CompanyRepNumber NVARCHAR(255) NOT NULL UNIQUE,
+    CompanyTaxId NVARCHAR(255) NOT NULL UNIQUE,
+    CompanyUsesErp BIT NOT NULL UNIQUE,
+    CompanyWebsite NVARCHAR(255) NOT NULL UNIQUE,
+    CorporateUserId UNIQUEIDENTIFIER NOT NULL UNIQUE,
+    ErpBankingActivities NVARCHAR(255) NOT NULL,
+    ProductCategoriesId UNIQUEIDENTIFIER NOT NULL
 );
--- Database/query_2.sql
+++ Database/query_2.sql
@@ -1,7 +1,11 @@
 
-
--- SupportCategories Table
-CREATE TABLE SupportCategories (
+-- File: ProductCategories.sql
+CREATE TABLE ProductCategories (
     Id UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
-    Name NVARCHAR(200) NOT NULL
-)
+    Name NVARCHAR(200) NOT NULL UNIQUE,
+    UserQuestionnaire BIT NULL,
+    Description NVARCHAR(MAX) NULL,
+    Parent NVARCHAR(MAX) NULL,
+    UrlAlias NVARCHAR(200) NULL,
+    Weight INT NULL
+);

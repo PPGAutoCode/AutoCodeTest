da53b9a81dcbd55f2804ba9f4972a6df9156c8b9
AutoCode <autocode@papergo.io>
2024-06-28 03:34:12 +0300

    update from autocode on 2024-06-28 03:34:12.873031

--- obj/Debug/net6.0/ProjectName.AssemblyInfo.cs
+++ obj/Debug/net6.0/ProjectName.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectName")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d64df170eeac9c7dfcc6eebd52405ec091e69ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectName")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectName")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

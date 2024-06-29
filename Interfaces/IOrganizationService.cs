4ae00140f9a4858ad0803ac72c093fc3382a55f2
AutoCode <autocode@papergo.io>
2024-06-29 21:02:58 +0300

    update from autocode on 2024-06-29 21:02:58.765668

--- Interfaces/IOrganizationService.cs
+++ Interfaces/IOrganizationService.cs
@@ -6,43 +6,43 @@ using ProjectName.Types;
 namespace ProjectName.Interfaces
 {
     /// <summary>
-    /// Service interface for managing organization-related operations.
+    /// Interface for managing organization-related operations.
     /// </summary>
     public interface IOrganizationService
     {
         /// <summary>
-        /// Creates a new organization based on the provided data.
+        /// Creates a new organization.
         /// </summary>
-        /// <param name="createOrganizationDto">Data transfer object containing the details for the new organization.</param>
+        /// <param name="createOrganizationDto">Data transfer object containing the details of the organization to be created.</param>
         /// <returns>A string representing the result of the creation operation.</returns>
         Task<string> CreateOrganization(CreateOrganizationDto createOrganizationDto);
 
         /// <summary>
-        /// Retrieves an organization based on the provided data.
+        /// Retrieves an organization based on the provided request details.
         /// </summary>
-        /// <param name="organizationDto">Data transfer object containing the details to identify the organization.</param>
-        /// <returns>An Organization object representing the retrieved organization.</returns>
-        Task<Organization> GetOrganization(OrganizationDto organizationDto);
+        /// <param name="organizationRequestDto">Data transfer object containing the request details to fetch an organization.</param>
+        /// <returns>An Organization object representing the fetched organization.</returns>
+        Task<Organization> GetOrganization(OrganizationRequestDto organizationRequestDto);
 
         /// <summary>
-        /// Updates an existing organization based on the provided data.
+        /// Updates an existing organization.
         /// </summary>
-        /// <param name="updateOrganizationDto">Data transfer object containing the details for the update.</param>
+        /// <param name="updateOrganizationDto">Data transfer object containing the details of the organization to be updated.</param>
         /// <returns>A string representing the result of the update operation.</returns>
         Task<string> UpdateOrganization(UpdateOrganizationDto updateOrganizationDto);
 
         /// <summary>
-        /// Deletes an organization based on the provided data.
+        /// Deletes an organization based on the provided details.
         /// </summary>
-        /// <param name="deleteOrganizationDto">Data transfer object containing the details to identify the organization to delete.</param>
-        /// <returns>A boolean indicating whether the deletion was successful.</returns>
+        /// <param name="deleteOrganizationDto">Data transfer object containing the details of the organization to be deleted.</param>
+        /// <returns>A boolean indicating the success or failure of the deletion operation.</returns>
         Task<bool> DeleteOrganization(DeleteOrganizationDto deleteOrganizationDto);
 
         /// <summary>
-        /// Retrieves a list of organizations based on the provided request data.
+        /// Retrieves a list of organizations based on the provided request details.
         /// </summary>
-        /// <param name="listOrganizationRequesteDto">Data transfer object containing the request details.</param>
-        /// <returns>A list of Organization objects representing the retrieved organizations.</returns>
-        Task<List<Organization>> GetListOrganization(ListOrganizationRequesteDto listOrganizationRequesteDto);
+        /// <param name="listOrganizationRequestDto">Data transfer object containing the request details to fetch a list of organizations.</param>
+        /// <returns>A list of Organization objects representing the fetched organizations.</returns>
+        Task<List<Organization>> GetListOrganization(ListOrganizationRequestDto listOrganizationRequestDto);
     }
 }

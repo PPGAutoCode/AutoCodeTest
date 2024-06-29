76c69e94723e85b705302f79ff47adc16e76a524
AutoCode <autocode@papergo.io>
2024-06-30 00:03:42 +0300

    update from autocode on 2024-06-30 00:03:42.043426

--- Types/Author.cs
+++ Types/Author.cs
@@ -5,6 +5,12 @@ namespace ProjectName.Types
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
-        public string? Details { get; set; }
+        public Guid? ImageId { get; set; }
+        public string Details { get; set; }
+        public int? Version { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Changed { get; set; }
+        public Guid CreatorId { get; set; }
+        public Guid ChangedUser { get; set; }
     }
 }
--- obj/Debug/net6.0/ProjectName.AssemblyInfo.cs
+++ obj/Debug/net6.0/ProjectName.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectName")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e39501508491b948fc740209ff7e9f7ab6fd90c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectName")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectName")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

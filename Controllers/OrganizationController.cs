6b36b3c79b8823b53d82c8cd68e4adb39f78e13d
AutoCode <autocode@papergo.io>
2024-06-29 21:04:51 +0300

    update from autocode on 2024-06-29 21:04:51.710973

--- Controllers/OrganizationController.cs
+++ Controllers/OrganizationController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Mvc;
 using ProjectName.Types;
 using ProjectName.Interfaces;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace ProjectName.Controllers
@@ -19,52 +18,52 @@ namespace ProjectName.Controllers
         }
 
         [HttpPost("create")]
-        public async Task<IActionResult> CreateOrganization([FromBody] CreateOrganizationDto request)
+        public async Task<IActionResult> CreateOrganization([FromBody] Request<CreateOrganizationDto> request)
         {
             return await SafeExecutor.ExecuteAsync(async () =>
             {
-                var result = await _organizationService.CreateOrganization(request);
-                return Ok(new Response<string>(result));
+                var result = await _organizationService.CreateOrganization(request.Payload);
+                return Ok(new Response<string> { Payload = result });
             });
         }
 
         [HttpPost("get")]
-        public async Task<IActionResult> GetOrganization([FromBody] OrganizationDto request)
+        public async Task<IActionResult> GetOrganization([FromBody] Request<OrganizationRequestDto> request)
         {
             return await SafeExecutor.ExecuteAsync(async () =>
             {
-                var result = await _organizationService.GetOrganization(request);
-                return Ok(new Response<Organization>(result));
+                var result = await _organizationService.GetOrganization(request.Payload);
+                return Ok(new Response<Organization> { Payload = result });
             });
         }
 
         [HttpPost("update")]
-        public async Task<IActionResult> UpdateOrganization([FromBody] UpdateOrganizationDto request)
+        public async Task<IActionResult> UpdateOrganization([FromBody] Request<UpdateOrganizationDto> request)
         {
             return await SafeExecutor.ExecuteAsync(async () =>
             {
-                var result = await _organizationService.UpdateOrganization(request);
-                return Ok(new Response<string>(result));
+                var result = await _organizationService.UpdateOrganization(request.Payload);
+                return Ok(new Response<string> { Payload = result });
             });
         }
 
         [HttpPost("delete")]
-        public async Task<IActionResult> DeleteOrganization([FromBody] DeleteOrganizationDto request)
+        public async Task<IActionResult> DeleteOrganization([FromBody] Request<DeleteOrganizationDto> request)
         {
             return await SafeExecutor.ExecuteAsync(async () =>
             {
-                var result = await _organizationService.DeleteOrganization(request);
-                return Ok(new Response<bool>(result));
+                var result = await _organizationService.DeleteOrganization(request.Payload);
+                return Ok(new Response<bool> { Payload = result });
             });
         }
 
         [HttpPost("list")]
-        public async Task<IActionResult> GetListOrganization([FromBody] ListOrganizationRequestDto request)
+        public async Task<IActionResult> GetListOrganization([FromBody] Request<ListOrganizationRequestDto> request)
         {
             return await SafeExecutor.ExecuteAsync(async () =>
             {
-                var result = await _organizationService.GetListOrganization(request);
-                return Ok(new Response<List<Organization>>(result));
+                var result = await _organizationService.GetListOrganization(request.Payload);
+                return Ok(new Response<List<Organization>> { Payload = result });
             });
         }
     }

db89f695a96faa445735bf9ac9f03b2bbf15d186
AutoCode <autocode@papergo.io>
2024-07-01 15:12:08 +0300

    update from autocode on 2024-07-01 15:12:08.978978

--- Types/BasicPage.cs
+++ Types/BasicPage.cs
@@ -1,6 +1,4 @@
 
-using System;
-
 namespace ProjectName.Types
 {
     public class BasicPage
@@ -8,10 +6,5 @@ namespace ProjectName.Types
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Body { get; set; }
-        public int? Version { get; set; }
-        public DateTime Created { get; set; }
-        public DateTime Changed { get; set; }
-        public Guid CreatorId { get; set; }
-        public Guid ChangedUser { get; set; }
     }
 }
--- Types/Image.cs
+++ Types/Image.cs
@@ -6,6 +6,11 @@ namespace ProjectName.Types
         public Guid Id { get; set; }
         public string FileName { get; set; }
         public byte[] ImageData { get; set; }
-        public string AltText { get; set; }
+        public string? AltText { get; set; }
+        public int? Version { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Changed { get; set; }
+        public Guid CreatorId { get; set; }
+        public Guid ChangedUser { get; set; }
     }
 }

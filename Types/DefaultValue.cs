75a39139514dbf5a467b94c305c2e842a512bd83
AutoCode <autocode@papergo.io>
2024-07-01 18:18:05 +0300

    update from autocode on 2024-07-01 18:18:05.302037

--- Types/DefaultValue.cs
+++ Types/DefaultValue.cs
@@ -1,13 +1,11 @@
 
-using System;
-
 namespace ProjectName.Types
 {
     public class DefaultValue
     {
-        public Guid Id { get; set; }
+        public Guid id { get; set; }
         public string RoleId { get; set; }
-        public string RoleName { get; set; }
+        public string? RoleName { get; set; }
         public string Description { get; set; }
     }
 }
--- Types/UserRole.cs
+++ Types/UserRole.cs
@@ -5,8 +5,9 @@ namespace ProjectName.Types
     {
         public Guid Id { get; set; }
         public string Label { get; set; }
-        public string HelpText { get; set; }
+        public string? HelpText { get; set; }
         public string ReferenceMethod { get; set; }
+        public List<Guid> DefaultValue { get; set; }
         public DateTime Created { get; set; }
         public DateTime Changed { get; set; }
     }

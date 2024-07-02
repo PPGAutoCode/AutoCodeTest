f3858b7414247920c795bbb655890f514d06cfc7
AutoCode <autocode@papergo.io>
2024-07-02 17:54:54 +0300

    update from autocode on 2024-07-02 17:54:54.364084

--- Types/APIEndpoint.cs
+++ Types/APIEndpoint.cs
@@ -4,21 +4,23 @@ namespace ProjectName.Types
     public class APIEndpoint
     {
         public Guid Id { get; set; }
+        public string Name { get; set; }
         public string ApiContext { get; set; }
         public string ApiReferenceId { get; set; }
         public string ApiResource { get; set; }
         public string ApiScope { get; set; }
         public string ApiScopeProduction { get; set; }
         public string ApiSecurity { get; set; }
-        public Guid ApiTags { get; set; }
+        public List<ApiTag> ApiTags { get; set; }
         public bool Deprecated { get; set; }
         public string Description { get; set; }
         public string Documentation { get; set; }
         public string EndpointUrls { get; set; }
         public string EnvironmentId { get; set; }
+        public string ProviderId { get; set; }
         public string Swagger { get; set; }
         public string Tour { get; set; }
-        public DateTime Updated { get; set; }
+        public DateTime? Updated { get; set; }
         public string Version { get; set; }
     }
 }

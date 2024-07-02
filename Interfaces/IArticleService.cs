9d52d16494b9c477c84f41e7cd7ad1ff5570983d
AutoCode <autocode@papergo.io>
2024-07-03 00:54:02 +0300

    update from autocode on 2024-07-03 00:54:02.939907

--- Interfaces/IArticleService.cs
+++ Interfaces/IArticleService.cs
@@ -1,5 +1,4 @@
 
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using ProjectName.Types;
 
@@ -11,38 +10,31 @@ namespace ProjectName.Interfaces
     public interface IArticleService
     {
         /// <summary>
-        /// Creates a new article.
+        /// Creates a new article based on the provided data.
         /// </summary>
-        /// <param name="createArticleDto">The data transfer object containing the details of the article to be created.</param>
+        /// <param name="createArticleDto">The data transfer object containing the article information to be created.</param>
         /// <returns>A string representing the result of the creation operation.</returns>
         Task<string> CreateArticle(CreateArticleDto createArticleDto);
 
         /// <summary>
-        /// Retrieves an article based on the provided request details.
+        /// Retrieves an article based on the provided request data.
         /// </summary>
-        /// <param name="articleRequestDto">The data transfer object containing the request details for the article.</param>
+        /// <param name="articleRequestDto">The data transfer object containing the request information to retrieve the article.</param>
         /// <returns>An Article object representing the retrieved article.</returns>
         Task<Article> GetArticle(ArticleRequestDto articleRequestDto);
 
         /// <summary>
-        /// Updates an existing article.
+        /// Updates an existing article based on the provided data.
         /// </summary>
-        /// <param name="updateArticleDto">The data transfer object containing the details of the article to be updated.</param>
+        /// <param name="updateArticleDto">The data transfer object containing the updated article information.</param>
         /// <returns>A string representing the result of the update operation.</returns>
         Task<string> UpdateArticle(UpdateArticleDto updateArticleDto);
 
         /// <summary>
-        /// Deletes an article based on the provided details.
+        /// Deletes an article based on the provided data.
         /// </summary>
-        /// <param name="deleteArticleDto">The data transfer object containing the details of the article to be deleted.</param>
+        /// <param name="deleteArticleDto">The data transfer object containing the information to delete the article.</param>
         /// <returns>A boolean indicating whether the deletion was successful.</returns>
         Task<bool> DeleteArticle(DeleteArticleDto deleteArticleDto);
-
-        /// <summary>
-        /// Retrieves a list of articles based on the provided request details.
-        /// </summary>
-        /// <param name="listArticleRequestDto">The data transfer object containing the request details for the list of articles.</param>
-        /// <returns>A list of Article objects representing the retrieved articles.</returns>
-        Task<List<Article>> GetListArticle(ListArticleRequestDto listArticleRequestDto);
     }
 }

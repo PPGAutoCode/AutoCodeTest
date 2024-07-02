f6f937f71e914affbeb3ebe6a041fd500b749440
AutoCode <autocode@papergo.io>
2024-07-03 00:52:12 +0300

    update from autocode on 2024-07-03 00:52:12.701714

--- Interfaces/IArticleService.cs
+++ Interfaces/IArticleService.cs
@@ -1,4 +1,5 @@
 
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using ProjectName.Types;
 
@@ -10,31 +11,38 @@ namespace ProjectName.Interfaces
     public interface IArticleService
     {
         /// <summary>
-        /// Creates a new article based on the provided data.
+        /// Creates a new article.
         /// </summary>
-        /// <param name="createArticleDto">Data transfer object containing the information needed to create an article.</param>
-        /// <returns>A string representing the identifier of the newly created article.</returns>
+        /// <param name="createArticleDto">The data transfer object containing the details of the article to be created.</param>
+        /// <returns>A string representing the result of the creation operation.</returns>
         Task<string> CreateArticle(CreateArticleDto createArticleDto);
 
         /// <summary>
-        /// Retrieves an article based on the provided request data.
+        /// Retrieves an article based on the provided request details.
         /// </summary>
-        /// <param name="articleRequestDto">Data transfer object containing the information needed to request an article.</param>
-        /// <returns>An Article object representing the requested article.</returns>
+        /// <param name="articleRequestDto">The data transfer object containing the request details for the article.</param>
+        /// <returns>An Article object representing the retrieved article.</returns>
         Task<Article> GetArticle(ArticleRequestDto articleRequestDto);
 
         /// <summary>
-        /// Updates an existing article based on the provided data.
+        /// Updates an existing article.
         /// </summary>
-        /// <param name="updateArticleDto">Data transfer object containing the information needed to update an article.</param>
-        /// <returns>A string representing the identifier of the updated article.</returns>
+        /// <param name="updateArticleDto">The data transfer object containing the details of the article to be updated.</param>
+        /// <returns>A string representing the result of the update operation.</returns>
         Task<string> UpdateArticle(UpdateArticleDto updateArticleDto);
 
         /// <summary>
-        /// Deletes an article based on the provided data.
+        /// Deletes an article based on the provided details.
         /// </summary>
-        /// <param name="deleteArticleDto">Data transfer object containing the information needed to delete an article.</param>
-        /// <returns>A boolean indicating whether the article was successfully deleted.</returns>
+        /// <param name="deleteArticleDto">The data transfer object containing the details of the article to be deleted.</param>
+        /// <returns>A boolean indicating whether the deletion was successful.</returns>
         Task<bool> DeleteArticle(DeleteArticleDto deleteArticleDto);
+
+        /// <summary>
+        /// Retrieves a list of articles based on the provided request details.
+        /// </summary>
+        /// <param name="listArticleRequestDto">The data transfer object containing the request details for the list of articles.</param>
+        /// <returns>A list of Article objects representing the retrieved articles.</returns>
+        Task<List<Article>> GetListArticle(ListArticleRequestDto listArticleRequestDto);
     }
 }
--- obj/Debug/net6.0/ProjectName.AssemblyInfo.cs
+++ obj/Debug/net6.0/ProjectName.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectName")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07456f772a3fa2a61efcf7a11f8b09d73fc23cc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectName")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectName")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
